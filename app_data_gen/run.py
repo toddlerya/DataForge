de9253f6d708e0e52be3fcb07075db807d5bba33
guoqun <qguo@njsecnet.com>
2025-07-04 09:38:57 +0800

    集成app为fastapi服务

---
+++ app_data_gen/run.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+# @Time     : 2025/7/4 9:23 
+# @Author   : guoqun X2590
+# @FileName : run.py
+# @Project  : DataForge
+
+
+from uvicorn import Server, Config
+from fastapi import FastAPI
+from chainlit.utils import mount_chainlit
+
+app = FastAPI()
+
+
+@app.get("/health")
+def read_main():
+    return {"message": "Hi"}
+
+
+mount_chainlit(app=app, target="app.py", path="/")
+
+
+if __name__ == '__main__':
+    Server(
+        Config(
+            app=app,
+            host='0.0.0.0',
+            port=9801,
+            workers=2
+        )
+    ).run()
