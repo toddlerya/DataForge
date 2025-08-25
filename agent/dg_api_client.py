544f19eddd86f820528c940b4d594bb18bebf596
guoqun <qguo@njsecnet.com>
2025-08-25 14:52:02 +0800

    提取出dg api调用作为独立模块复用

---
+++ agent/dg_api_client.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# coding: utf-8
+# @Time     : 2025/8/25 14:42 
+# @Author   : guoqun X2590
+# @FileName : dg_api_client.py
+# @Project  : DataForge
+
+import json
+import time
+from urllib.parse import urljoin
+from typing import Union
+
+import httpx
+
+from loguru import logger
+
+from config import DG_PLAN_PATH, DG_PAYLOAD_PATH
+from agent.state import (
+    DataGenState,
+    SQLModeDataGenState
+)
+
+from agent.dg_configs import (
+    DG_SERVER_BASE_URL,
+    DG_TASK_ADD_URL,
+    DG_TASK_HISTORY,
+    DG_NEW_TASK,
+)
+from utils.file import save_dict2jl
+
+
+def create_dg_task(state: Union[SQLModeDataGenState, DataGenState]
+                      ) -> Union[SQLModeDataGenState, DataGenState]:
+    """
+    创建人DataGenius任务
+    Args:
+        state:
+
+    Returns:
+
+    """
+    pydantic_data_genius_plan = state.get("pydantic_data_genius_plan")
+    user_intent = state["user_intent"]
+    data_genius_headers = state["data_genius_headers"]
+    table_en_name = user_intent.table_en_names[0]
+    logger.info(
+        f"创建DataGenius任务, 任务名称: {pydantic_data_genius_plan.rule_name} data_genius_headers: {data_genius_headers}"
+    )
+    pydantic_data_genius_plan_dict = pydantic_data_genius_plan.model_dump()
+    payload = {
+        "task": json.dumps(
+            {
+                "step": "2",
+                "name": pydantic_data_genius_plan.rule_name,
+                "type_": pydantic_data_genius_plan.type_,
+                "modelName": table_en_name,
+                "mode": "create",
+                "task_id": "None",
+                "duration": None,
+                "output_filesize": None,
+            }
+        ),
+        "rules": json.dumps(pydantic_data_genius_plan_dict["rules"]),
+        "separator": pydantic_data_genius_plan.separator,
+        "rows": pydantic_data_genius_plan.rows,
+        "cols": pydantic_data_genius_plan.cols,
+        "send": json.dumps(
+            {
+                "send_type": 1,
+                "id": None,
+                "tip": "无配置，点击刷新或添加。",
+                "connect_test": False,
+                "connect_test_tip": "",
+                "table_name": "",
+                "table_test": False,
+                "table_test_tip": "",
+                "table_columns": [],
+                "schemas": "public",
+            }
+        ),
+        "saveRuleFile": False,
+        "blockSize": 100000,
+        "source": "",
+        "alam": json.dumps({"isRule": "1", "rule": "", "name": ""}),
+    }
+
+    save_json_path = DG_PAYLOAD_PATH.joinpath(
+        f"payload_{pydantic_data_genius_plan.rule_name}"
+    ).absolute()
+    save_dict2jl(json_data=payload, save_path=str(save_json_path))
+    create_task_url = urljoin(DG_SERVER_BASE_URL, DG_TASK_ADD_URL)
+
+    with httpx.Client() as client:
+        response = client.post(
+            create_task_url, data=payload, headers=data_genius_headers
+        )
+    if response.status_code != 200:
+        logger.error(f"请求{create_task_url}异常, status_code: {response.status_code}")
+        state["create_data_genius_task_error"] = (
+            f"请求{create_task_url}异常, status_code: {response.status_code}"
+        )
+        logger.debug(
+            f"state.create_data_genius_task_error: {state['create_data_genius_task_error']}"
+        )
+    return state
+
+
+def query_dg_task_status(state: Union[SQLModeDataGenState, DataGenState]
+                      ) -> Union[SQLModeDataGenState, DataGenState]:
+    """
+    查询当前任务状态
+    Args:
+        state:
+
+    Returns:
+
+    """
+    pydantic_data_genius_plan = state.get("pydantic_data_genius_plan")
+    logger.info(f"查询DataGenius进度, 任务名称: {pydantic_data_genius_plan.rule_name}")
+    query_task_url = urljoin(DG_SERVER_BASE_URL, DG_TASK_HISTORY)
+    payload = {"limit": 10}
+    data_genius_headers = state["data_genius_headers"]
+    with httpx.Client() as client:
+        for _ in range(60):
+            response = client.get(
+                query_task_url, params=payload, headers=data_genius_headers
+            )
+            if response.status_code != 200:
+                logger.error(
+                    f"请求{query_task_url}异常, status_code: {response.status_code}"
+                )
+                state["query_data_genius_task_error"] = (
+                    f"请求{query_task_url}异常, status_code: {response.status_code}"
+                )
+                return state
+            resp_json = response.json()
+            for result in resp_json.get("results", [{}]):
+                if result.get("name", "") == pydantic_data_genius_plan.rule_name:
+                    if result.get("status_name") == "成功":
+                        logger.trace(f"matched result: {result}")
+                        task_id = result.get("task_id", "not_found_task_id")
+                        duration = result.get("duration_", "not_found_duration")
+                        # 拼接URL
+                        output_url = urljoin(
+                            DG_SERVER_BASE_URL,
+                            result.get("output", "not_found_output_url"),
+                        )
+                        output_filesize = result.get(
+                            "output_filesize", "not_found_output_filesize"
+                        )
+                        data_genius_plan_edit_url = (
+                            f"{DG_SERVER_BASE_URL}/{DG_NEW_TASK}?"
+                            f"step=2&"
+                            f"name={pydantic_data_genius_plan.rule_name}&"
+                            f"type_={pydantic_data_genius_plan.type_}&"
+                            f"modelName={pydantic_data_genius_plan.model}&"
+                            f"mode=edit&"
+                            f"task_id={task_id}"
+                        )
+                        logger.debug(
+                            f"duration: {duration}\n output_url: {output_url}\n "
+                            f"output_filesize: {output_filesize}\n"
+                            f"data_genius_plan_edit_url: {data_genius_plan_edit_url}"
+                        )
+                        state["data_genius_plan_task_id"] = task_id
+                        state["data_genius_plan_run_duration"] = duration
+                        state["data_genius_plan_output_url"] = output_url
+                        state["data_genius_plan_output_filesize"] = output_filesize
+                        state["data_genius_plan_edit_url"] = data_genius_plan_edit_url
+                        return state
+            time.sleep(2)
+    return state
