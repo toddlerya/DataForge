fdc3c9c96970eb4734d3f2a0e6518a3501761eb1
guoqun <qguo@njsecnet.com>
2025-08-29 16:55:04 +0800

    格式化代码，消除部分lint警告

--- server/api/schemas/base_schema.py
+++ server/api/schemas/base_schema.py
@@ -17,7 +17,11 @@ class ResponseBaseSchema(BaseModel):
     """响应体结构定义"""
 
     description: str = Field(..., description="描述信息")
-    code: str = Field(error_code.DEFAULT.get("code"), description="响应数据系统状态码")
-    message: str = Field(error_code.DEFAULT.get("description"), description="错误信息")
+    code: str = Field(
+        error_code.DEFAULT.get("code", ""), description="响应数据系统状态码"
+    )
+    message: str = Field(
+        error_code.DEFAULT.get("description", ""), description="错误信息"
+    )
     data: Any = Field(None, description="响应数据内容")
     session_id: str = Field("", description="会话ID")
