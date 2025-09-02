478ae895363a2e3cc4a72f704522d934537305b1
guoqun <qguo@njsecnet.com>
2025-09-02 15:09:48 +0800

    更新dg的headers

--- agent/dg_api_client.py
+++ agent/dg_api_client.py
@@ -43,7 +43,7 @@ def create_dg_task(
     pydantic_data_genius_plan = state.get("pydantic_data_genius_plan")
     user_intent = state["user_intent"]
     client_ip = state["client_ip"]
-    state["data_genius_headers"] = {"USER_PROVIDE_IP": client_ip}
+    state["data_genius_headers"] = {"SPECIFIEDIP": client_ip}
     data_genius_headers = state["data_genius_headers"]
     if isinstance(user_intent, DataGenUserIntentSchema):
         table_en_name = user_intent.table_en_names[0]
