a68449bd0a1cd1db5f6513df41c6c49ac33e5fd7
guoqun <qguo@njsecnet.com>
2025-10-10 12:37:23 +0800

    优化输出信息

--- app_agent/app.py
+++ app_agent/app.py
@@ -234,7 +234,6 @@ async def handle_graph_event(node: str, state: dict, run_config: RunnableConfig)
             ]
             if table_metadata_elements:
                 await cl.Message(
-                    author="Database",
                     content=f"{table_metadata_info.table_en_name}表字段信息",
                     elements=table_metadata_elements,
                 ).send()
@@ -294,7 +293,6 @@ async def handle_graph_event(node: str, state: dict, run_config: RunnableConfig)
                 )
             ]
             await cl.Message(
-                author="Database",
                 content=f"{table_info_data.table_en_name}表字段信息",
                 elements=table_metadata_elements,
             ).send()
@@ -306,7 +304,7 @@ async def handle_graph_event(node: str, state: dict, run_config: RunnableConfig)
                 PydanticDataGeniusPlan, pydantic_data_genius_plan
             )
             await cl.Message(
-                content=("已生成的DataGenius数据生成计划配置, 将开始数据生成任务"),
+                content=("已生成DataGenius任务配置, 将开始数据生成任务"),
             ).send()
             # await cl.Message(
             #     content=pydantic_data_genius_plan.model_dump_json(indent=2),
